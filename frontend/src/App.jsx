f8ffebbd36f3fe52a249017ceaf669de1d32eaed
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2025-07-23 12:53:02 -0400

    created Navbar which switches pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,19 @@ import './App.css'
 import Home from './pages/home';
 import Favorites from './pages/favorites';
 import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+
 function App() {
   return (
-      <>
+      <div>
+        <NavBar/>
       <main className='main-content'>
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/favorites' element={<Favorites />}/>
         </Routes>
       </main>
-      </>
+      </div>
   );
 }
 

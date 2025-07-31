982e510617998ab417fbd90fb6d28580b710302d
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2025-07-31 14:55:58 -0400

    making favorites function

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,21 +1,27 @@
-
 import "../css/MovieCard.css"
+import { useMovieContext } from "../contexts/MovieContext"
+
 
 
 // component for displaying movie
 
 const MovieCard = ({movie}) => {
 
-    const onFavoriteClick =() =>{
-        alert("clicked")
+    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext()
+    const favorite = isFavorite(movie.id)
+
+    const onFavoriteClick =(e) =>{
+       e.preventDefault()
+       if (favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
     }
 	
     return <div className="movie-card">
         <div className="movie-poster">
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
             <div className="movie-overlay">
-            <button className="favorite-btn" onClick={onFavoriteClick}>
-                🤍
+            <button className={`favorite-btn ${favorite ? "active" :""}`} onClick={onFavoriteClick}>
+                {favorite ? "❤️" : "🤍"}
             </button>
             </div>
         </div>
